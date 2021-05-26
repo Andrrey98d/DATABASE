bb27db75fd401b9ed463ef8daa580fc6fb184725
Andrrey98d <60302268+Andrrey98d@users.noreply.github.com>
2021-05-26 15:16:25 +0300

    deleted at 108

--- DATABASE/Program.cs
+++ DATABASE/Program.cs
@@ -22,46 +22,46 @@ namespace DATABASE
         public static string SELECT_ALL = "SELECT * FROM dbo.Table_1";
         public static string SEL_ALL = "SELECT * FROM Users";
         public const string CNCT = "Data Source = usersdata.db";
-        public const string ALL_TABLES = ". tables"; //list all tables in database 
+        public const string ALL_TABLES = ".tables"; //list all tables in database 
         public static SqliteConnection sqn = new SqliteConnection(CNCT);
         public static SqliteConnection SQN = new SqliteConnection(CNCT);
         public static OpenFileDialog ofd = new OpenFileDialog();
         public static SaveFileDialog sfd = new SaveFileDialog();
-
         [STAThread] // in case if using GUI
-        public static void Main(string [] args)
+        public static void Main(string[] args)
         {
+
             Console.WriteLine(CNCT + "\n");
             Task task_con = new Task(Initialize_Connection);
             task_con.Start();
             ShowMenu();
             int res = Convert.ToInt32(Console.ReadLine());
-                switch (res)
-                {
-                    /*Во всех случаях меняется только sql-выражение*/
-                    case 1:
-                        for (int i = 0; i <= 2; i++)
-                        {
-                            Console.Write(".");
-                        }
-                        DataTable_Name();
-                        break;
-                    case 2:
-                        Create_NewTable();
-                        break;
-                    case 3:
-                        UPDATE();
-                        break;
-                    case 4:
-                        INSERT_VALUES();
-                        break;
-                    case 5:
-                        INSERT_FEW();
-                        break;
-                    case 6:
-                        DELETE();
-                        break;
-                }
+            switch (res)
+            {
+                /*Во всех случаях меняется только sql-выражение*/
+                case 1:
+                    for (int i = 0; i <= 2; i++)
+                    {
+                        Console.Write(".");
+                    }
+                    DataTable_Name();
+                    break;
+                case 2:
+                    Create_NewTable();
+                    break;
+                case 3:
+                    UPDATE();
+                    break;
+                case 4:
+                    INSERT_VALUES();
+                    break;
+                case 5:
+                    INSERT_FEW();
+                    break;
+                case 6:
+                    DELETE();
+                    break;
+            }
         }
         public static void Initialize_Connection()
         {
@@ -80,66 +80,32 @@ namespace DATABASE
                         {
                             while (reader.Read()) //counter++ => removed await method to avoid non-recognising point of entry (Main)
                             {
-                                string text = "";
-                                string[] words = text.Split(' ');
-                                try
+                                bool HR = reader.HasRows;
+                                if (HR == true)
                                 {
-                                    string w1 = words[0];
-                                    string w2 = words[1];
-                                    string w3 = words[2];
-                                }
-                                catch (IndexOutOfRangeException)
-                                {
-                                    Console.WriteLine("Index was out of range.");
-                                }
-                                List<string> ID = new List<string>();
-                                var id = reader.GetValue(0);
-                                object[] objs = new object[1]; //1,2,3,  и т.д
-                                //string[] column1_values = dtTemp1.AsEnumerable().Select(s => s.Field<string>("City")).ToArray<string>(); //где dtTemp1 - экзмепляр datatable
-                                var column_val = reader.GetValues(objs); //берем все значения столбцов с второй строки 
-                                var column_values = column_val.ToString();
-                                string result_ = "";
-
-                                foreach (var id_ in column_values)
-                                {
-                                    result_ += id_ + "\n"; //можно разобрать массив и через сплит, но не суть
-                                }
-                                foreach (var value in Convert.ToString(id))
-                                {
-                                    var COLUMN_ROW = reader.GetString(value);
-                                    //_ = dt.AsEnumerable().Select(r => r.Field<int>("id")).ToList();
-                                    ID.Add(COLUMN_ROW); // заносим в лист наши значения со столбца [0]
-                                }
-                                string[] ids = ID.ToArray();
-                                for (int i = 0; i > ids.Length; i++)
-                                {
-                                    using (StreamWriter sw = new StreamWriter(path, false, Encoding.Default)) //перезапись. True - дозапись
+                                    string text = "";
+                                    string[] words = text.Split(' ');
+                                    for (int i = 0; i < reader.FieldCount; i++)
                                     {
-                                        sw.WriteLine(ids);
+                                        Console.Write("\t" + reader[i]);
                                     }
-                                    using (ZipFile zip = new ZipFile())
+                                    Console.WriteLine("");
+                                    try
                                     {
-                                        using (Archive archive = new Archive()) //создаем архив + преобразовать эту функцию в преобразование по GUID
-                                        {
-                                            if (sfd.ShowDialog() == DialogResult.Cancel)
-                                            {
-                                                return;
-                                            }
-                                            string FileName = sfd.FileName;
-                                            zip.AddFile(ids.ToString());
-                                            DataTable_Name();
-                                            string zip_path = @"D://" + /*datatable.name*/ (FileName) + ".7z"; // сюда вместо элемента листа [0] можно впихнуть чето типо "selected button" <= wf
-                                            archive.Save(zip_path, new ArchiveSaveOptions { Encoding = Encoding.ASCII, ArchiveComment = result_ }); // or Encoding
-
-                                            //"Добавлен новый файл в архив, guid"
-                                            //один архив - один гуид(вся строка, привязанная к нему), в нем будут лежать еще файлы (dta или другие)
-                                            //соответственно, в папке будут лежать n архивов = n строк(гуидов). 7 гуидов(или строк) => 7 архивов и т.д
-                                            //и соответственно, количество таблиц = количество папок, в которых буду лежать архивы с гуидами. 7 таблиц(tables) = 7 папок
-                                            //path для папок = название таблицы
-                                            //path для архивов = файловая херня (там будет столбец с названиями, типо type_material и т.д), вот их и вбивать в путь, перед 
-                                        }
+                                        string w1 = words[0];
+                                        string w2 = words[1];
+                                        string w3 = words[2];
+                                    }                                 
+                                    catch (IndexOutOfRangeException)
+                                    {
+                                        Console.WriteLine("Index was out of range.");
                                     }
-                                    Console.WriteLine("{0}", id);
+                                    List<string> ID = new List<string>();
+                                    var id = reader.GetValue(0);
+
+                                    object[] objs = new object[1]; //1,2,3,  и т.д
+                                                                   //string[] column1_values = dtTemp1.AsEnumerable().Select(s => s.Field<string>("City")).ToArray<string>(); //где dtTemp1 - экземпляр datatable
+                                   }
                                 }
                             }
                         }
@@ -148,8 +114,8 @@ namespace DATABASE
                 }
                 Console.Read();
             }
-        }
-    public static void Create_NewTable()
+        
+        public static void Create_NewTable()
         {
             //users.db лежит  в src/*proj name*/bin/debug
             var sqn = new SqliteConnection(CNCT);
@@ -170,7 +136,7 @@ namespace DATABASE
             }
             Console.Read();
         }
-    public static void INSERT_VALUES()
+        public static void INSERT_VALUES()
         {
             // добавить ридер по столбцам, прирaвняв его к переменной (var => reader[0])
             using (SQN)
@@ -186,7 +152,7 @@ namespace DATABASE
             }
             Console.Read();
         }
-    public static void INSERT_FEW()  //primer: INSERT INTO Users (Name, Age) VALUES ('Alice', 32), ('Bob', 28)";
+        public static void INSERT_FEW()  //primer: INSERT INTO Users (Name, Age) VALUES ('Alice', 32), ('Bob', 28)";
         {
             Console.WriteLine("INSERT NAME: ");
             string Name = Console.ReadLine(); ;
@@ -239,7 +205,7 @@ namespace DATABASE
         }
         public static void DataTable_Name()
         {
-            List <string> tables = new List <string>();
+            List<string> tables = new List<string>();
             using (SqliteConnection con = new SqliteConnection(CNCT))
             {
                 con.Open();
